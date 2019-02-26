269eb81440e2e3edd023ea4888c2a83172e12e80
grybouilli <grybouilli>
2019-02-26 07:33:27 +0100

    new files for bottom collision

---
+++ src/collidesBottom.cpp
@@ -0,0 +1,8 @@
+#include "collidesBottom.hpp"
+
+bool collidesBottom(sf::FloatRect collider, sf::FloatRect collided)
+{
+	return !(collided.left + collided.width < collider.left || collided.left > collider.left + collider.width)
+	&& collided.top + collided.height < collider.top + collider.height
+	&& collided.top + collided.height >= collider.top;
+}
