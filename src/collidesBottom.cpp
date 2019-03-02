f49c8ef2eeb7f170a2b60dca1c9bb9bdc9b1ff28
grybouilli <grybouilli>
2019-03-02 15:58:47 +0100

    ultimate bottom collision fix

--- src/collidesBottom.cpp
+++ src/collidesBottom.cpp
@@ -2,6 +2,6 @@
 bool collidesBottom(sf::FloatRect collider, sf::FloatRect collided)
 {
 	return !(collided.left + collided.width < collider.left || collided.left > collider.left + collider.width)
-	&& collided.top + collided.height < collider.top + collider.height
+	&& collided.top + collided.height < collider.top + 10.f
 	&& collided.top + collided.height >= collider.top;
 }
